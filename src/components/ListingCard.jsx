5ca1124029e618839eb6f09fe7822288176c15d6
Haris Majeed <bscs22017@itu.edu.pk>
2024-10-05 22:46:13 +0500

    take images from airbnb site and use those images in project

--- src/components/ListingCard.jsx
+++ src/components/ListingCard.jsx
@@ -29,15 +29,16 @@
 import PropTypes from 'prop-types'; // Import PropTypes
 import '../styles/ListingCard.css';
 
-const ListingCard = ({ image, title, type, guests, bedrooms, bathrooms, price, rating }) => {
+const ListingCard = ({ image, title, hostedby, price, status }) => {
     return (
         <div className="listing-card">
             <img src={image} alt={title} />
             <div className="listing-info">
                 <h3>{title}</h3>
-                <p>{type} • {guests} guests • {bedrooms} bedrooms • {bathrooms} bathrooms</p>
-                <p className="price">${price} per night</p>
-                {rating && <p>Rating: {rating}</p>} {/* Conditionally render rating if available */}
+                <p className="hostedby">{hostedby}</p>
+                {/* <p>{type} • {guests} guests • {bedrooms} bedrooms • {bathrooms} bathrooms</p> */}
+                <p className="price">{price}{status}</p>
+                {/* {rating && <p>Rating: {rating}</p>}  */}
             </div>
         </div>
     );
@@ -46,11 +47,13 @@ const ListingCard = ({ image, title, type, guests, bedrooms, bathrooms, price, r
 ListingCard.propTypes = {
     image: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
+    hostedby: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
     guests: PropTypes.number.isRequired,
     bedrooms: PropTypes.number.isRequired,
     bathrooms: PropTypes.number.isRequired,
-    price: PropTypes.number.isRequired,
+    price: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
     rating: PropTypes.number.isRequired,
 };
 
